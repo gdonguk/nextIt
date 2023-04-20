6f86c11e87be59bef9b3121ad70b3b02373e7bb5
pc08 <pc08@PC08>
2023-04-20 12:45:30 +0900

    됬나 ?

--- javaStudy/src/ch11_api/ApiJson.java
+++ javaStudy/src/ch11_api/ApiJson.java
@@ -1,8 +1,13 @@
 package ch11_api;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
 public class ApiJson {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws ParseException {
 		// 객체 지향 프로그래밍 (OOP)
 		// 개발 대상, 데이터 등을 객체로 보고
 		// 객체로 만들어서 구현 
@@ -40,9 +45,81 @@ public class ApiJson {
 		// - Add Jars 클릭 - 프로젝트 내 library 폴도를 찾아
 		// 해당 .jar 파일을 선택해준다. 
 		
- 
+		// 라이브러리 만들기 어렵지 않다. 대단한게 아니다.
 		
 		
+		// JSON 객체 만들기 
+		// HashMap<String, Object>과 사용법 유사 
+		JSONObject json = new JSONObject();
+		
+		json.put("name", "이수");
+		json.put("age", 40);
+		
+		// JSONArray 리스트 생성
+		JSONArray songList = new JSONArray();
+		songList.add("원러브");
+		songList.add("그대가분다");
+		songList.add("마이웨이");
+		
+		System.out.println(songList);
+		
+		json.put("노래목록", songList);
+		
+		System.out.println(json);
+		System.out.println(json.toJSONString());
+		System.out.println(json.toString());
+		
+		String jsonStr = json.toJSONString();
+		System.out.println(jsonStr);
+		
+		System.out.println("\n====================================\n");
+		
+		JSONObject daysix = new JSONObject();
+		daysix.put("이름", "데이식스");
+		daysix.put("노래", "좀비");
+		
+		json.put("동료", daysix);
+		
+		System.out.println(json);
+		
+		// 값 꺼내보기
+		System.out.println(json.get("name"));
+		String name = (String) json.get("name");
+		System.out.println(name);
+		
+		int age = (int) json.get("age");
+		System.out.println(age);
+		
+		// 없는 키값에 대해서는 null을 리턴한다. 
+		JSONArray temp = (JSONArray) json.get("노래목록");
+		System.out.println(temp);
+		System.out.println(temp.get(2));
+		System.out.println("\n=========================\n");
+		
+		// 서버에서 온 JSONString 데이터를 JSONObject 객체로 
+		// 바꾼 후 키에 대한 값 꺼내기 !!
+		String serverDate = "{\"노래목록\":[\"원러브\",\"그대가분다\",\"마이웨이\"],\"동료\":{\"노래\":\"좀비\",\"이름\":\"데이식스\"},\"name\":\"이수\",\"age\":40}";
+		System.out.println(serverDate);
+		
+		// JSONParser를 이용해서 파싱 
+		JSONParser parser = new JSONParser();
+		
+		JSONObject result = (JSONObject) parser.parse(serverDate);
+		System.out.println(result);
+		
+		name = (String) result.get("name");
+		System.out.println(name);
+		
+		// 파싱 후 숫자는 long 타입으로 꺼내와야 한다. 
+		long ageL = (long) result.get("age");
+		System.out.println(ageL);
+		
+		// result로 부터 "좀비" 꺼내서 출력하기
+		JSONObject asd = (JSONObject) result.get("동료");
+		String aaa = (String) asd.get("노래");
+		System.out.println(aaa);
+	
+	
 	}
 
 }
